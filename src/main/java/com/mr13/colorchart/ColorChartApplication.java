6eef3e7fbce21f2bcd92526946f18fa61716ed11
Maksim Rezchikov <maxim13rezchikov@gmail.com>
2020-03-28 17:36:35 +0300

    Start of app

---
+++ src/main/java/com/mr13/colorchart/ColorChartApplication.java
@@ -0,0 +1,13 @@
+package com.mr13.colorchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorChartApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ColorChartApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/mr13/colorchart/ColorChartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr13.colorchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorChartApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
